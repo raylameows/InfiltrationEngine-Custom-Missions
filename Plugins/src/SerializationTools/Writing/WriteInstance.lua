7b1fa8c6811b53512686bd4399d17dcad78b568d
Rayla <knownasrayla@gmail.com>
2026-04-10 05:49:08 +0200

    Symmetrize WriteInstance.lua with ReadInstance.lua

--- Plugins/src/SerializationTools/Writing/WriteInstance.lua
+++ Plugins/src/SerializationTools/Writing/WriteInstance.lua
@@ -3,7 +3,12 @@ local InstanceProperties = require(script.Parent.Parent.Types.InstanceProperties
 local AttributeTypes = require(script.Parent.Parent.Types.AttributeTypes)
 local AttributeValidation = require(script.Parent.Parent.AttributeValidation)
 
-local function lookupMapIndex(map, value)
+local WriteInstance
+local DefaultFlags = { -- Some instances may use additional features, those are enabled via flags
+	Attributes = false, -- Whether the attributes of the instance should be serialized
+}
+
+local function LookupMapIndex(map, value)
 	if value == nil then
 		return 0
 	end
@@ -16,61 +21,16 @@ local function lookupMapIndex(map, value)
 		map[value] = idx
 		map[0] = idx -- Update size
 	end
+	
 	return idx
 end
 
-local WithAttributes = function(DefaultWriter)
-	return function(object, Write, colorMap, stringMap)
-		local str = DefaultWriter(object, Write, colorMap, stringMap)
-		local attributes = object:GetAttributes()
-		attributes = AttributeValidation.Validate(object.ClassName, object.Name, attributes, false)
-		local attString = ""
-
-		-- Encoding Attributes
-		for k, v in pairs(attributes) do
-			if k:match("^RBX_") then
-				continue
-			end
-
-			local attributeType = typeof(v) -- Changing attribute type names to match as they are in the Write file
-			if typeof(v) == "number" then
-				if v ~= math.round(v) or v < 0 then
-					attributeType = "Float"
-				else
-					attributeType = "LongInt"
-				end
-			elseif typeof(v) == "boolean" then
-				attributeType = "bool"
-			end
-			attributeType = (string.upper(string.sub(attributeType, 1, 1)) .. string.sub(attributeType, 2, -1))
-			if AttributeTypes[attributeType] == nil then -- if the attribute is not in the table, ignore it
-				continue
-			end
-
-			local index = lookupMapIndex(stringMap, k)
-			attString = attString
-				.. StringConversion.NumberToString(AttributeTypes[attributeType], 1)
-				.. Write.ShortInt(index)
-
-			if attributeType == "Color3" then
-				local index = lookupMapIndex(colorMap, v)
-				attString = attString .. Write.ShortInt(index)
-			elseif attributeType == "String" then
-				local index = lookupMapIndex(stringMap, v)
-				attString = attString .. Write.ShortInt(index)
-			else
-				attString = attString .. Write[attributeType](v)
-			end
-		end
-		str = str .. attString .. StringConversion.NumberToString(0, 1)
-		return str, colorMap, stringMap
-	end
-end
-
-local CreateInstanceWriter = function(properties)
-	local WriteInstance = function(object, Write, colorMap, stringMap)
-		local str = ""
-		for i, v in pairs(properties) do
+local function CreateInstanceWriter(properties, flags)
+	if not flags then flags = DefaultFlags end
+	
+	local InstanceWriter = function(object, Write, colorMap, stringMap)
+		local chunks = {}
+		for i, v in (properties) do
 			local value
 			if v[1] == "MeshId" and object.ClassName == "UnionOperation" then
 				value = object:GetAttribute("MeshId")
@@ -80,39 +40,81 @@ local CreateInstanceWriter = function(properties)
 			local valueType = v[2]
 			local defaultValue = v[3]
 			if (valueType == "Color3") and (value ~= defaultValue) then
-				local index = lookupMapIndex(colorMap, value)
-				str = str .. StringConversion.NumberToString(i, 1)
-				str = str .. Write.ShortInt(index)
+				local index = LookupMapIndex(colorMap, value)
+				table.insert(chunks, StringConversion.NumberToString(i, 1))
+				table.insert(chunks, Write.ShortInt(index))
 				continue
 			elseif (valueType == "String") and (value ~= defaultValue) then
-				local index = lookupMapIndex(stringMap, value)
-				str = str .. StringConversion.NumberToString(i, 1)
-				str = str .. Write.ShortInt(index)
+				local index = LookupMapIndex(stringMap, value)
+				table.insert(chunks, StringConversion.NumberToString(i, 1))
+				table.insert(chunks, Write.ShortInt(index))
 				continue
 			elseif value ~= defaultValue then
-				str = str .. StringConversion.NumberToString(i, 1)
-				str = str .. Write[valueType](value)
+				table.insert(chunks, StringConversion.NumberToString(i, 1))
+				table.insert(chunks, Write[valueType](value))
 			end
 		end
+		table.insert(chunks, StringConversion.NumberToString(0, 1)) -- Mark end of property serialization for this Instance
+		
+		if flags.Attributes then
+			local attributes = object:GetAttributes()
+			attributes = AttributeValidation.Validate(object.ClassName, object.Name, attributes, false)
 
-		str = str .. StringConversion.NumberToString(0, 1)
+			-- Encoding Attributes
+			for k, v in (attributes) do
+				if k:match("^RBX_") then
+					continue
+				end
+
+				local attributeType = typeof(v) -- Changing attribute type names to match as they are in the Write file
+				if attributeType == "number" then
+					if v ~= math.round(v) or v < 0 then
+						attributeType = "Float"
+					else
+						attributeType = "LongInt"
+					end
+				elseif attributeType == "boolean" then
+					attributeType = "bool"
+				end
+				attributeType = (string.upper(string.sub(attributeType, 1, 1)) .. string.sub(attributeType, 2, -1))
+				if AttributeTypes[attributeType] == nil then -- if the attribute is not in the table, ignore it
+					continue
+				end
+
+				local index = LookupMapIndex(stringMap, k)
+				table.insert(chunks, StringConversion.NumberToString(AttributeTypes[attributeType], 1))
+				table.insert(chunks, Write.ShortInt(index))
+
+				if attributeType == "Color3" then
+					local index = LookupMapIndex(colorMap, v)
+					table.insert(chunks, Write.ShortInt(index))
+				elseif attributeType == "String" then
+					local index = LookupMapIndex(stringMap, v)
+					table.insert(chunks, Write.ShortInt(index))
+				else
+					table.insert(chunks, Write[attributeType](v))
+				end
+			end
+			table.insert(chunks, StringConversion.NumberToString(0, 1)) -- Mark end of attribute serialization for this Instance
+		end
+
+		local str = table.concat(chunks)
 		return str, colorMap, stringMap
 	end
-	return WriteInstance
+	
+	return InstanceWriter
 end
 
-local WriteInstance
-
 WriteInstance = {
-	Model = WithAttributes(CreateInstanceWriter(InstanceProperties.Model)),
-	Folder = WithAttributes(CreateInstanceWriter(InstanceProperties.Folder)),
-	Part = WithAttributes(CreateInstanceWriter(InstanceProperties.Part)),
+	Model = CreateInstanceWriter(InstanceProperties.Model, {Attributes = true}),
+	Folder = CreateInstanceWriter(InstanceProperties.Folder, {Attributes = true}),
+	Part = CreateInstanceWriter(InstanceProperties.Part, {Attributes = true}),
 	PartNoAttributes = CreateInstanceWriter(InstanceProperties.Part),
-	BoolValue = WithAttributes(CreateInstanceWriter(InstanceProperties.BoolValue)),
+	BoolValue = CreateInstanceWriter(InstanceProperties.BoolValue, {Attributes = true}),
 	WedgePart = CreateInstanceWriter(InstanceProperties.WedgePart),
 	StringValue = CreateInstanceWriter(InstanceProperties.StringValue),
-	MeshPart = WithAttributes(CreateInstanceWriter(InstanceProperties.MeshPart)),
-	UnionOperation = WithAttributes(CreateInstanceWriter(InstanceProperties.UnionOperation)),
+	MeshPart = CreateInstanceWriter(InstanceProperties.MeshPart, {Attributes = true}),
+	UnionOperation = CreateInstanceWriter(InstanceProperties.UnionOperation, {Attributes = true}),
 	Texture = CreateInstanceWriter(InstanceProperties.Texture),
 	BlockMesh = CreateInstanceWriter(InstanceProperties.BlockMesh),
 	PointLight = CreateInstanceWriter(InstanceProperties.PointLight),
